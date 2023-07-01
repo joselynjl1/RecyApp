58e6f2d4dbd68eddc70c34856e313c494d4bcc0e
Christophervc <christophervc122@gmail.com>
2023-07-01 01:45:23 -0500

    Initial commit

---
+++ app/src/main/java/com/example/recyapp/UI/Activities/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.recyapp.UI.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.recyapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}

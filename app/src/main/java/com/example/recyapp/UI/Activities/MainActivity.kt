b91d30462424053a1efc847f18f394437cf5c44d
joselynjl1 <jhoselyn.12.13lopez@gmail>
2023-07-24 03:02:25 -0500

    Commit realizado

--- app/src/main/java/com/example/recyapp/UI/Activities/MainActivity.kt
+++ app/src/main/java/com/example/recyapp/UI/Activities/MainActivity.kt
@@ -1,4 +1,5 @@
 package com.example.recyapp.UI.Activities
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
@@ -6,19 +7,42 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.example.recyapp.R
 import com.example.recyapp.UI.Fragments.EditUserFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    /*private lateinit var binding: ActivityMainBinding*/
+    private lateinit var navController:NavController
+    private lateinit var bottomNavigation: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //MUESTRA LOS TRES PUNTITOS DE (EDITAR Y CERRAR SESION)
         val toolbar = findViewById<Toolbar>(R.id.TBMain)
         setSupportActionBar(toolbar)
+
+        bottomNavigation=findViewById(R.id.BTMNavigation)
+        navController=findNavController(R.id.fragmentInicio)
+        bottomNavigation.setupWithNavController(navController)
+
+        /*val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_frag) as NavHostFragment
+
+        navController = navHostFragment.navController
+
+        // Configurar la ActionBar con el NavController
+        NavigationUI.setupActionBarWithNavController(this, navController)*/
+
 
         /*
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -53,16 +77,15 @@ class MainActivity : AppCompatActivity() {
                 cambiarFragmentEditar(fragment = EditUserFragment())
                 true
             }
-
             R.id.cerrar_sesion -> {
                 val intent= Intent(this, LoginActivity::class.java)
                 startActivity(intent)
                 true
             }
-
             else -> super.onOptionsItemSelected(item)
         }
     }
+
 
     private fun cambiarFragmentEditar(fragment: EditUserFragment){
         val fragmentManager: FragmentManager = supportFragmentManager
@@ -71,4 +94,9 @@ class MainActivity : AppCompatActivity() {
         fragmentTransaction.addToBackStack(null)
         fragmentTransaction.commit()
     }
+/*
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }*/
+
 }

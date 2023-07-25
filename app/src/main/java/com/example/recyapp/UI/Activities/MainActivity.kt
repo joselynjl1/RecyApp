87f96e4d354204e70edc0f323ca6f17b60c6a407
joselynjl1 <jhoselyn.12.13lopez@gmail>
2023-07-25 00:58:09 -0500

    Commit realizado

--- app/src/main/java/com/example/recyapp/UI/Activities/MainActivity.kt
+++ app/src/main/java/com/example/recyapp/UI/Activities/MainActivity.kt
@@ -1,18 +1,12 @@
 package com.example.recyapp.UI.Activities
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.example.recyapp.R
 import com.example.recyapp.UI.Fragments.EditUserFragment
@@ -20,49 +14,17 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var navController:NavController
-    private lateinit var bottomNavigation: BottomNavigationView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //MUESTRA LOS TRES PUNTITOS DE (EDITAR Y CERRAR SESION)
-        val toolbar = findViewById<Toolbar>(R.id.TBMain)
-        setSupportActionBar(toolbar)
+        //NAVEGACION DE LA APP
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentInicio) as NavHostFragment
+        val navController = navHostFragment.navController
+        // Configurar el BottomNavigationView para que refleje la navegación
+        val bottomNavView = findViewById<BottomNavigationView>(R.id.BTMNavigation)
+        bottomNavView.setupWithNavController(navController)
 
-        /*
-        bottomNavigation=findViewById(R.id.BTMNavigation)
-        navController=findNavController(R.id.fragmentInicio)
-        bottomNavigation.setupWithNavController(navController)*/
-
-        /*val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_frag) as NavHostFragment
-
-        navController = navHostFragment.navController
-
-        // Configurar la ActionBar con el NavController
-        NavigationUI.setupActionBarWithNavController(this, navController)*/
-
-
-        /*
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-
-        replaceFragment(Home())
-        binding.BTMNavigation.setOnItemSelectedListener {
-            when(it.itemId){
-                R.id.NAVHomeFragment->replaceFragment(Home())
-                R.id.NavInformacion->replaceFragment(Informacion())
-                R.id.NAVMapFragment->replaceFragment(Map())
-                R.id.NAVRegistroMaterial->replaceFragment(Registro())
-                R.id.NavRecompensas->replaceFragment(Recompensas())
-                else->{
-                }
-            }
-            true
-         */
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -87,7 +49,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
     private fun cambiarFragmentEditar(fragment: EditUserFragment){
         val fragmentManager: FragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
@@ -95,9 +56,4 @@ class MainActivity : AppCompatActivity() {
         fragmentTransaction.addToBackStack(null)
         fragmentTransaction.commit()
     }
-/*
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
-    }*/
-
 }

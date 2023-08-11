88725dcd766498eabfe5e508b30d47e556cafe17
joselynjl1 <jhoselyn.12.13lopez@gmail>
2023-08-11 13:01:18 -0500

    Commit realizado11-08 JoselynMapa

--- app/src/main/java/com/example/recyapp/UI/Fragments/MapaFragment.kt
+++ app/src/main/java/com/example/recyapp/UI/Fragments/MapaFragment.kt
@@ -6,21 +6,49 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.recyapp.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MapaFragment : Fragment() {
+class MapaFragment : Fragment(), OnMapReadyCallback {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_mapa, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val mapFragment = childFragmentManager.findFragmentById(R.id.mapaEstaciones) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        val ubicacion= LatLng(-9.126987719526605, -78.52198645412764)
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(ubicacion)
+                .title("Punto de recolección"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ubicacion,16f))
+
+        googleMap.uiSettings.isZoomControlsEnabled = true
+
+        googleMap.setMapStyle(
+            MapStyleOptions.loadRawResourceStyle(
+                requireContext(), R.raw.estilomapa
+            )
+        )
+    }
 
 }
